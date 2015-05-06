82719f36452aa5b8b26fb54f8a8e51081c7f86e1
SummersBrian <summers.brian.cs@gmail.com>
2015-05-05 23:51:16 -0400

    Slider mostly done

---
+++ Swiveler/Assets/_Scripts/PlatformDrag.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDrag : MonoBehaviour {
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private float slideMax, slideMin;
+	private enum Orientation{Vertical, Horizontal};
+	private Orientation myOrientation;
+	private bool canSlide;
+	private bool dragging;
+	private Transform toDrag;
+
+	void Start() {
+		dragging = false;
+	}
+	
+	void Update(){
+		Vector3 v3;
+		if (canSlide && Input.GetMouseButtonDown (0)) {
+			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+			
+			if (hit.collider != null) {
+				toDrag = transform;
+				screenPoint = Camera.main.WorldToScreenPoint (toDrag.position);
+				v3 = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+				offset = toDrag.position - v3;
+				dragging = true;
+
+			}
+		}
+		if (Input.GetMouseButton(0)) {
+
+			if (canSlide && dragging) {
+			v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			v3 = Camera.main.ScreenToWorldPoint(v3);
+			setOrientation ();
+			if (myOrientation == Orientation.Horizontal) {
+				if (v3.x > slideMax) {
+					v3.x = slideMax - transform.lossyScale.x / 2.0f;
+				} else if (v3.x < slideMin){
+					v3.x = slideMin - transform.lossyScale.x / 2.0f;
+				} else {
+					v3.x += offset.x;
+				}
+				toDrag.position = new Vector3(v3.x, transform.position.y, transform.position.z);
+				//transform.position = new Vector3(v3.x, lockedPosition);
+			} else {
+				if (v3.y > slideMax) {
+					v3.y = slideMax - transform.lossyScale.y / 2.0f;
+				} else if (v3.y < slideMin){
+					v3.y = slideMin - transform.lossyScale.y / 2.0f;
+				} else {
+					v3.y += offset.y;
+				}
+				toDrag.position = new Vector3(toDrag.position.x, v3.y + offset.y, transform.position.z);
+				}
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			if (dragging) {
+				dragging = false;
+				float delta1, delta2;
+				if (myOrientation == Orientation.Horizontal) {
+					delta1 = transform.position.x - slideMin;
+					delta2 = transform.position.x - slideMax + (transform.lossyScale.x / 2.0f);
+					if (delta1 * delta1 <= delta2 * delta2) {
+						transform.position = new Vector3 (slideMin - transform.lossyScale.x / 2.0f, transform.position.y, transform.position.z);
+					} else {
+						transform.position = new Vector3 (slideMax - transform.lossyScale.x / 2.0f, transform.position.y, transform.position.z);
+					}
+					SlidebarConnector sb = transform.GetComponentInParent<SlidebarConnector>();
+					if (sb.swapOwnerOfPlatform(transform.position.x, transform.position.y)) {
+						transform.SetParent(sb.connectedSlideBar.transform, true);
+					}
+
+				} else {
+					delta1 = transform.position.y - slideMin;
+					delta2 = transform.position.y - slideMax;
+					if (delta1 * delta1 <= delta2 * delta2) {
+						transform.position = new Vector3 (transform.position.x, slideMin - transform.lossyScale.y / 2.0f, transform.position.z);
+					} else {
+						transform.position = new Vector3 (transform.position.x, slideMax - transform.lossyScale.y / 2.0f, transform.position.z);
+					}
+				}
+			}
+		}
+
+		}
+	
+	void setOrientation() {
+		if (gameObject.transform.eulerAngles.z == 180 || gameObject.transform.eulerAngles.z == 0) {
+			myOrientation = Orientation.Horizontal;
+		} else {
+			myOrientation = Orientation.Vertical;
+		}
+	}
+
+	public void setSlideMax(float val) {
+		slideMax = val;
+	}
+
+	public void setSlideMin(float val) {
+		slideMin = val;
+	}
+
+	public void CanSlide(bool b) {
+		canSlide = b;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.transform.tag == "Player")
+			canSlide = false;
+	}
+
+	void OnCollisionExit2D(Collision2D collision) {
+		if (collision.transform.tag == "Player")
+			canSlide = true;
+	}
+	
+}
---
+++ Swiveler/Assets/_Scripts/SlidebarConnector.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidebarConnector : MonoBehaviour {
+
+	public GameObject platform;
+	public GameObject platformSlot;
+	public GameObject rotatingSquare;
+	
+	public enum Orientation{Vertical,Horizontal};
+	private Orientation myOrientation;
+	private BoxCollider2D myCollider;
+	private float max;
+	private float min;
+	public GameObject connectedPlatformSlot;
+	public SlidebarConnector connectedSlideBar;
+
+	void Start() {
+		//setOrientation ();
+		myCollider = GetComponent<BoxCollider2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		findOrientation ();
+		if (platform != null) {
+			PlatformDrag platformScript = platform.GetComponent<PlatformDrag>();
+			connectedSlideBar = collider.GetComponent<SlidebarConnector>();
+			connectedPlatformSlot = connectedSlideBar.platformSlot;
+			connectedSlideBar.connectedSlideBar = GetComponent<SlidebarConnector>();
+			connectedSlideBar.connectedPlatformSlot = platformSlot;
+			if (collider.transform.tag == "SlideBar") {
+				connectedPlatformSlot = collider.GetComponent<SlidebarConnector>().platformSlot;
+				platformScript.CanSlide(true);
+				//setOrientation ();
+				if (myOrientation == Orientation.Horizontal) {
+					if (myCollider.bounds.min.x < collider.bounds.min.x) {
+						//min = myCollider.bounds.min.x;
+						//max = collider.bounds.max.x;
+						min = platformSlot.transform.position.x;
+						max = connectedPlatformSlot.transform.position.x;
+					} else {
+						//min = collider.bounds.min.x;
+						//max = myCollider.bounds.max.x;
+						min = connectedPlatformSlot.transform.position.x;
+						max = platformSlot.transform.position.x;
+					}
+				} else {
+					if (myCollider.bounds.min.y < collider.bounds.min.y) {
+						min = myCollider.bounds.min.y;
+						max = collider.bounds.max.y;
+					} else {
+						min = collider.bounds.min.y;
+						max = myCollider.bounds.max.y;
+					}
+				}
+
+				platformScript.setSlideMax(max);
+				platformScript.setSlideMin(min);
+			} 
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider) {
+		findOrientation ();
+		if (collider.gameObject.CompareTag("SlideBar")) {
+			connectedSlideBar = null;
+			if (myOrientation == Orientation.Horizontal) {
+				min = myCollider.bounds.min.x;
+				max = myCollider.bounds.max.x;
+			} else {
+				min = myCollider.bounds.min.y;
+				max = myCollider.bounds.max.y;
+			}
+		
+			if (platform != null) {
+				PlatformDrag platformScript = platform.GetComponent<PlatformDrag>();
+				platformScript.CanSlide(false);
+				if (myOrientation == Orientation.Horizontal) {
+					platformScript.setSlideMin(myCollider.bounds.min.x - (collider.transform.localScale.x /2.0f));
+					platformScript.setSlideMax(myCollider.bounds.min.x - (collider.transform.localScale.x /2.0f));
+				} else {
+					platformScript.setSlideMin(collider.bounds.min.y);
+					platformScript.setSlideMax(collider.bounds.max.y);
+				}
+			} 
+		}
+	}
+
+	void findOrientation() {
+		if (rotatingSquare.transform.localEulerAngles.z == 180.0f || rotatingSquare.transform.localEulerAngles.z == 0.0f) {
+			myOrientation = Orientation.Horizontal;
+		} else {
+			myOrientation = Orientation.Vertical;
+		}
+	}
+
+	public bool swapOwnerOfPlatform(float x, float y) {
+		float delta1, delta2;
+		if (myOrientation == Orientation.Horizontal) {
+			delta1 = x - platformSlot.transform.position.x;
+			delta2 = x - connectedPlatformSlot.transform.position.x;
+			if (delta1 * delta1 > delta2 * delta2) {
+			//if (x > myCollider.bounds.max.x || x < myCollider.bounds.min.x) {
+				connectedSlideBar.platform = platform;
+				platform = null;
+				return true;
+			}
+			else return false;
+		} else {
+			if (y > myCollider.bounds.max.y || y < myCollider.bounds.min.y) {
+				connectedSlideBar.platform = this.platform;
+				platform = null;
+				return true;
+			} else return false;
+		}
+	}
+}
